4a78ce59dbe0f45621333405c5a0bad058d63705
Hameshphy <hameshyadav301702@gmail.com>
2024-03-27 21:48:05 +0530

    add

---
+++ app.js
@@ -0,0 +1,15 @@
+Shery.mouseFollower({
+    //Parameters are optional.
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 0.8,
+  });
+  Shery.makeMagnet("#nav h1,.navright span,.navright a,.btn button" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 0.8,
+  });
+  Shery.hoverWithMediaCircle(".homemidtext h1" /* Element to target.*/, {
+    videos: ["pexels-artem-podrez-5725954 (720p).mp4", "4057150-hd_720_1366_50fps (1).mp4", "4057150-hd_720_1366_50fps.mp4"]
+  
+  });
